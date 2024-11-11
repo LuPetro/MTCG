31b8c2af5b1d4ae5a2078684d28aaf93b4c27f9e
Luka Petrovic <lukamatej17@gmail.com>
2024-11-11 13:43:50 +0100

    Updated Cards with more logic

--- SpellCard.cs
+++ SpellCard.cs
@@ -6,9 +6,48 @@ using System.Threading.Tasks;
 
 namespace Swen1.MTCG_Petrovic
 {
-    public class SpellCard : Card
+    public class SpellCard : Card, IBattlable
     {
         public SpellCard(string name, int damage, string element)
             : base(name, damage, element) { }
+
+        public int CalculateDamageAgainst(Card opponent)
+        {
+            // Spezialregel: Kraken sind immun gegen Zauber
+            if (opponent.Name == "Kraken")
+            {
+                return 0;  // Kraken nimmt keinen Schaden von Zaubern
+            }
+
+            // Spezialregel: Ritter ertrinken bei Wassersprüchen
+            if (this.Element == "water" && opponent.Name == "Knight")
+            {
+                return int.MaxValue;  // Maximaler Schaden, um den Ritter sofort zu besiegen
+            }
+
+            // Spezialregel: FireElves weichen Drachen aus
+            if (this.Element == "fire" && opponent.Name == "Dragon" && this.Name == "FireElf")
+            {
+                return 0;  // FireElf nimmt keinen Schaden von Drachen
+            }
+
+            // Berechnung der Elementareffektivität
+            int effectiveDamage = this.Damage;
+            if ((this.Element == "water" && opponent.Element == "fire") ||
+                (this.Element == "fire" && opponent.Element == "normal") ||
+                (this.Element == "normal" && opponent.Element == "water"))
+            {
+                effectiveDamage *= 2;  // Verdoppelter Schaden bei effektiven Angriffen
+            }
+            else if ((this.Element == "fire" && opponent.Element == "water") ||
+                     (this.Element == "normal" && opponent.Element == "fire") ||
+                     (this.Element == "water" && opponent.Element == "normal"))
+            {
+                effectiveDamage /= 2;  // Halbierter Schaden bei ineffektiven Angriffen
+            }
+
+            return effectiveDamage;
+        }
     }
+
 }
