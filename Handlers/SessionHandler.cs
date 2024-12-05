315126f8ebb707341738f37038ae225fb8d58db8
Luka Petrovic <lukamatej17@gmail.com>
2024-12-05 19:19:09 +0100

    Changed Case in json

--- Handlers/SessionHandler.cs
+++ Handlers/SessionHandler.cs
@@ -26,8 +26,8 @@ namespace Swen1.MTCG_Petrovic.Handlers
                 JsonNode? json = JsonNode.Parse(e.Payload);
                 if (json != null)
                 {
-                    string username = (string)json["username"]!;
-                    string password = (string)json["password"]!;
+                    string username = (string)json["Username"]!;
+                    string password = (string)json["Password"]!;
 
                     (bool success, Models.User? user) = Models.User.Authenticate(username, password);
 
